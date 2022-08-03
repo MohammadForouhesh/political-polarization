1b878fef0b87e0f718101b3afa25fcedbd26d5e4
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-08-03 20:41:17 +0430

    Update Representatives_fixingBooleanFormatting.py

--- src/Representatives_fixingBooleanFormatting.py
+++ src/Representatives_fixingBooleanFormatting.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import pandas as pd
+
+
 rep_df = pd.read_csv('representatives_updated.csv', 'representatives_updated',delimiter=',',)
 user_ver = []
 reply = []
@@ -17,20 +13,12 @@ for i in rep_df['user_verified']:
         user_ver.append("False")
 rep_df = rep_df.drop(columns=['user_verified'])
 
-
-# In[5]:
-
-
 for i in rep_df['reply']:
     if i == 1:
         reply.append("True")
     else:
         reply.append("False")
 rep_df = rep_df.drop(columns=['reply'])
-
-
-# In[6]:
-
 
 for i in rep_df['retweet']:
     if i == 1:
@@ -39,20 +27,12 @@ for i in rep_df['retweet']:
         retweet.append("False")
 rep_df = rep_df.drop(columns=['retweet'])
 
-
-# In[7]:
-
-
 for i in rep_df['mention']:
     if i == 1:
         mention.append("True")
     else:
         mention.append("False")
 rep_df = rep_df.drop(columns=['mention'])
-
-
-# In[8]:
-
 
 replen = len(rep_df.loc[0])
 rep_df.insert(replen-1,"mention",mention,False)
@@ -61,9 +41,4 @@ rep_df.insert(replen-1,"reply",reply,False)
 rep_df.insert(replen-1,"user_verified",user_ver,False)
 rep_df.head()
 
-
-# In[9]:
-
-
 rep_df.to_csv('representatives_updated_tweet_type.csv')
-
