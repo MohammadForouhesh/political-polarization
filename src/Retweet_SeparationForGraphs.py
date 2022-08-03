fc8a6daf066339d29923466237192a0b7defd7d3
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-08-03 20:41:26 +0430

    Update Retweet_SeparationForGraphs.py

--- src/Retweet_SeparationForGraphs.py
+++ src/Retweet_SeparationForGraphs.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[14]:
-
-
 import pandas as pd
 import numpy as py
+
 retweet_df = pd.read_csv('retweet_tweets_sentiment.csv', 'retweet_tweets_sentiment',delimiter=',')
-
-
-# In[15]:
-
 
 keepcols = ["source_handle", "source_affiliation", "destination_handle", "destination_affiliation", "polarity", "subjectivity"] 
 
@@ -23,10 +14,6 @@ dem_rep = retweet_df.copy()
 dem_dem = retweet_df.copy()
 rep_rep = retweet_df.copy()
 rep_dem = retweet_df.copy()
-
-
-# In[16]:
-
 
 #list to drop all unnecessary rows; rows with dest_affil with nulls or Dems
 rowdrop = []
@@ -41,10 +28,6 @@ for d in dem_rep['destination_affiliation']:
     rindex = rindex+1
 dem_rep = dem_rep.drop(rowdrop)
 
-
-# In[17]:
-
-
 #list to drop all unnecessary rows; rows with dest_affil with nulls or Dems
 rowdrop = []
 rindex = 0
@@ -57,10 +40,6 @@ for d in dem_dem['destination_affiliation']:
         rowdrop.append(rindex)
     rindex = rindex+1
 dem_dem = dem_dem.drop(rowdrop)
-
-
-# In[18]:
-
 
 #list to drop all unnecessary rows; rows with dest_affil with nulls or Dems
 rowdrop = []
@@ -75,10 +54,6 @@ for d in rep_rep['destination_affiliation']:
     rindex = rindex+1
 rep_rep = rep_rep.drop(rowdrop)
 
-
-# In[19]:
-
-
 #list to drop all unnecessary rows; rows with dest_affil with nulls or Dems
 rowdrop = []
 rindex = 0
@@ -92,27 +67,16 @@ for d in rep_dem['destination_affiliation']:
     rindex = rindex+1
 rep_dem = rep_dem.drop(rowdrop)
 
-
-# In[20]:
-
-
 dem_rep = dem_rep.drop(columns = ['source_affiliation', 'destination_affiliation', 'subjectivity'])
 dem_dem = dem_dem.drop(columns = ['source_affiliation', 'destination_affiliation', 'subjectivity'])
 rep_rep = rep_rep.drop(columns = ['source_affiliation', 'destination_affiliation', 'subjectivity'])
 rep_dem = rep_dem.drop(columns = ['source_affiliation', 'destination_affiliation', 'subjectivity'])
 
 
-# In[21]:
-
-
 dem_rep = dem_rep.rename(columns={'source_handle': 'Source', 'destination_handle':'Target', 'polarity':'regulation'})
 dem_dem = dem_dem.rename(columns={'source_handle': 'Source', 'destination_handle':'Target', 'polarity':'regulation'})
 rep_rep = rep_rep.rename(columns={'source_handle': 'Source', 'destination_handle':'Target', 'polarity':'regulation'})
 rep_dem = rep_dem.rename(columns={'source_handle': 'Source', 'destination_handle':'Target', 'polarity':'regulation'})
-
-
-# In[22]:
-
 
 reg = []
 for i in dem_rep['regulation']:
@@ -133,10 +97,6 @@ for i in dem_rep['regulation']:
 dem_rep = dem_rep.drop(columns=['regulation'])
 dem_rep.insert(2,"regulation",reg,False)
 
-
-# In[23]:
-
-
 reg = []
 for i in dem_dem['regulation']:
     if i < 0 and i > -0.3:
@@ -155,10 +115,6 @@ for i in dem_dem['regulation']:
         reg.append(i)
 dem_dem = dem_dem.drop(columns=['regulation'])
 dem_dem.insert(2,"regulation",reg,False)
-
-
-# In[24]:
-
 
 reg = []
 for i in rep_rep['regulation']:
@@ -179,10 +135,6 @@ for i in rep_rep['regulation']:
 rep_rep = rep_rep.drop(columns=['regulation'])
 rep_rep.insert(2,"regulation",reg,False)
 
-
-# In[25]:
-
-
 reg = []
 for i in rep_dem['regulation']:
     if i < 0 and i > -0.3:
@@ -201,10 +153,6 @@ for i in rep_dem['regulation']:
         reg.append(i)
 rep_dem = rep_dem.drop(columns=['regulation'])
 rep_dem.insert(2,"regulation",reg,False)
-
-
-# In[26]:
-
 
 dem_rep.to_csv('retweet_dem_rep.csv', index=False)
 dem_dem.to_csv('retweet_dem_dem.csv', index=False)
