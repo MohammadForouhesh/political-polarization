c281c3e9dc4ba958bbff1e2d78215494e7a5b13b
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-08-03 20:41:13 +0430

    Update Representatives_Adding_Political_Affiliation.py

--- src/Representatives_Adding_Political_Affiliation.py
+++ src/Representatives_Adding_Political_Affiliation.py
@@ -1,28 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
+
 rep_df = pd.read_csv('representatives_updated_tweet_type.csv', 'representatives_updated_h',delimiter=',')
-
-
-# In[2]:
-
 
 rep_df.head()
 
-
-# In[3]:
-
-
 legis_df = pd.read_csv('legislators-current.csv', 'legislators-current',delimiter=',')
-
-
-# In[4]:
-
 
 legisdict = {}
 j = 0
@@ -39,18 +22,10 @@ for index, row in legis_df.iterrows():
 print(len(legisdict))
 print(len(legis_df))
 
-
-# In[5]:
-
-
 for i in legisdict:
     print("Key:",i)
     print("Party:",legisdict[i])
     print("---------------------")
-
-
-# In[6]:
-
 
 #testing dictionary
 j = 0
@@ -71,36 +46,16 @@ for i in rep_df['source_handle']:
         print("Party: NO AFFILIATION FOUND")
         print(rep_df['user_name'][j][5:])
 
-
-# In[7]:
-
-
 print(len(source_affiliation))
 print(len(rep_df['source_handle']))
-
-
-# In[8]:
-
 
 dfLen = len(rep_df.loc[0])
 rep_df.insert(dfLen-1, "source_affiliation", source_affiliation, False)
 
-
-# In[10]:
-
-
 rep_df.head()
-
-
-# In[17]:
-
 
 print(rep_df['destination_handle'][0])
 nvar = rep_df['destination_handle'][0]
-
-
-# In[21]:
-
 
 j = 0
 dest_affiliation = []
@@ -124,29 +79,14 @@ for i in rep_df['destination_handle']:
 #print(dest_affiliation)
 #print(legisdict[rep_df['destination_handle'][0]])
 
-
-# In[22]:
-
-
 print(len(dest_affiliation))
 print(len(null_affiliation))
-
-
-# In[23]:
-
 
 dfLen = len(rep_df.loc[0])
 rep_df.insert(dfLen, "destination_affiliation", dest_affiliation, False)
 
 
-# In[25]:
-
-
 rep_df.head(30)
-
-
-# In[27]:
-
 
 rep_df.to_csv('representatives_affiliations.csv', index=False)
 
